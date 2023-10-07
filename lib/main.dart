b0337f79a82597457b2407191945f5533e7dfc83
Babak <babakcode@gmail.com>
2023-10-07 18:46:27 +0330

    add list for first and second page

--- lib/main.dart
+++ lib/main.dart
@@ -38,189 +38,285 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final height = MediaQuery.sizeOf(context).height;
+
     return Scaffold(
       backgroundColor: Colors.blue.shade50,
-      body: Stack(
-        children: [
-          O3D(
-            src: 'assets/disney_style_character.glb',
-            controller: o3dController,
-            ar: false,
-            autoPlay: true,
-            autoRotate: false,
-            cameraControls: false,
-            cameraTarget: CameraTarget(-.25, 1.5, 1.5),
-            cameraOrbit: CameraOrbit(0, 90, 1),
-          ),
-          PageView(
-            controller: mainPageController,
-            children: [
-              const Center(),
-              const Center(),
-              ClipPath(
-                clipper: InvertedCircleClipper(),
-                child: Container(
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ),
-          Container(
-            width: 100,
-            height: double.infinity,
-            margin: const EdgeInsets.all(12),
-            child: PageView(
-              controller: textsPageController,
-              physics: const NeverScrollableScrollPhysics(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            O3D(
+              src: 'assets/disney_style_character.glb',
+              controller: o3dController,
+              ar: false,
+              autoPlay: true,
+              autoRotate: false,
+              cameraControls: false,
+              cameraTarget: CameraTarget(-.25, 1.5, 1.5),
+              cameraOrbit: CameraOrbit(0, 90, 1),
+            ),
+            PageView(
+              controller: mainPageController,
               children: [
-                Column(
-                  children: [
-                    const SizedBox(
-                      width: double.infinity,
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text("Daily goals"),
-                      ),
+                ListView.builder(
+                  padding: EdgeInsets.fromLTRB(12, height * 0.8, 12, 100),
+                  itemCount: 100,
+                  itemBuilder: (context, index) => Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset('assets/image1.jpg',
+                          fit: BoxFit.cover,
+                          width: 70,
+                          height: 70,
+                        ),
+                        const Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('A simple way to stay healthy',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text('Dr Babak',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          ),
+                        ),
+
+                        const Icon(Icons.location_on,
+                          color: Colors.red,
+                        )
+
+                      ],
                     ),
-                    SizedBox(
-                      width: double.infinity,
+                  ),
+                ),),
+                ListView.builder(
+                  padding: EdgeInsets.fromLTRB(12, height * 0.8, 12, 100),
+                  itemCount: 100,
+                  itemBuilder: (context, index) => Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(32.0),
                       child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Image.asset('assets/image2.jpg',
+                            fit: BoxFit.cover,
+                            width: 70,
+                            height: 70,
+                          ),
                           const Expanded(
-                            child: FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: Text("87"),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('10:24',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12
+                                    ),
+                                  ),
+                                  Text('Morning walk',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text('2 km in 30min',
+                                    style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                          Transform.translate(
-                              offset: const Offset(0, 20),
-                              child: const Text("%"))
+
+                          const Icon(Icons.directions_walk_rounded,
+                            color: Colors.red,
+                          )
+
                         ],
                       ),
                     ),
-                    const Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Icon(Icons.local_fire_department_outlined,
-                              color: Colors.red),
-                        ),
-                        Expanded(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("1,840"),
-                            Text(
-                              "calories",
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.grey),
-                            ),
-                          ],
-                        ))
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    const Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Icon(Icons.do_not_step, color: Colors.purple),
-                        ),
-                        Expanded(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("3,480"),
-                            Text(
-                              "steps",
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.grey),
-                            ),
-                          ],
-                        ))
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    const Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Icon(Icons.hourglass_bottom,
-                              color: Colors.lightBlueAccent),
-                        ),
-                        Expanded(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("6.5"),
-                            Text(
-                              "hours",
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.grey),
-                            ),
-                          ],
-                        ))
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      width: double.infinity,
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text("Journal"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Row(
-                        children: [
-                          Transform.translate(
-                              offset: const Offset(0, 20),
-                              child: const Text("<")),
-                          const Expanded(
-                            child: FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: Text("12"),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Text(
-                      "July 2020",
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ],
-                ),
-                const Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text("Profile"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text("Dis"),
-                      ),
-                    ),
-                    Text(
-                      "23 years old",
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ],
-                ),
+                  ),),
+                ClipPath(
+                  clipper: InvertedCircleClipper(),
+                  child: Container(
+                    color: Colors.white,
+                  ),
+                )
               ],
             ),
-          )
-        ],
+            Container(
+              width: 100,
+              height: double.infinity,
+              margin: const EdgeInsets.all(12),
+              child: PageView(
+                controller: textsPageController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(
+                        width: double.infinity,
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Daily goals"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            const Expanded(
+                              child: FittedBox(
+                                fit: BoxFit.fitWidth,
+                                child: Text("87"),
+                              ),
+                            ),
+                            Transform.translate(
+                                offset: const Offset(0, 20),
+                                child: const Text("%"))
+                          ],
+                        ),
+                      ),
+                      const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Icon(Icons.local_fire_department_outlined,
+                                color: Colors.red),
+                          ),
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("1,840"),
+                              Text(
+                                "calories",
+                                style:
+                                    TextStyle(fontSize: 12, color: Colors.grey),
+                              ),
+                            ],
+                          ))
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Icon(Icons.do_not_step, color: Colors.purple),
+                          ),
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("3,480"),
+                              Text(
+                                "steps",
+                                style:
+                                    TextStyle(fontSize: 12, color: Colors.grey),
+                              ),
+                            ],
+                          ))
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Icon(Icons.hourglass_bottom,
+                                color: Colors.lightBlueAccent),
+                          ),
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("6.5"),
+                              Text(
+                                "hours",
+                                style:
+                                    TextStyle(fontSize: 12, color: Colors.grey),
+                              ),
+                            ],
+                          ))
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(
+                        width: double.infinity,
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Journal"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          children: [
+                            Transform.translate(
+                                offset: const Offset(0, 20),
+                                child: const Text("<")),
+                            const Expanded(
+                              child: FittedBox(
+                                fit: BoxFit.fitWidth,
+                                child: Text("12"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        "July 2020",
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  const Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Profile"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Dis"),
+                        ),
+                      ),
+                      Text(
+                        "23 years old",
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: page,
